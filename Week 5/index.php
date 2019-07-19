6144017343f28ea908a00c1ef5d8a46edd1a40d7
BabaYaga007 <achint.179301016@muj.manipal.edu>
2019-07-20 00:19:58 +0530

    Done

---
+++ Week 5/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Achint Srivastava PHP</title>
+</head>
+<body>
+	<h1>Achint Srivastava PHP</h1>
+
+<pre>
+	        *
+	       * *
+	      *   *
+	     *     *
+	    *********
+	   *         *
+	  *           *
+	 *             *
+	*               *
+</pre>
+
+	<?php
+		print hash('sha256','Achint Srivastava');
+	?>
+
+</body>
+</html>
